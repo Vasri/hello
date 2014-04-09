21647a1962b7eb6e4930f1c3643c92f1d655bbdb
Vasri <dyang310@gmail.com>
2014-04-09 09:48:00 -0700

    Add an author/email comment

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ Public class Hello
   public static void main(String argv[])
   {
     // Default is "World"
+    // Author: Daniel Yang (dyang310@gmail.com)
     String name = "World";
     if ( argv.length != 0 )
     {
